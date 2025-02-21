45c1bc8cad31ae9ce4daf6b7bd10f29db983e119
Nisha Dodawad <nishadodawad5@gmail.com>
2025-02-21 20:22:37 +0530

    Create 125. Valid Palindrome.java

---
+++ Leetcode/125. Valid Palindrome.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.isEmpty())
+        {
+            return true;
+        }
+        int n=s.length();
+        int i=0, j=n-1;
+        while(i<j)
+        {
+            char c1 = s.charAt(i);
+            char c2 = s.charAt(j);
+            if(!Character.isLetterOrDigit(c1))
+            {
+                i++;
+            }
+            else if(!Character.isLetterOrDigit(c2))
+            {
+                j--;
+            }
+            else
+            {
+                if(!(Character.toLowerCase(c1)==Character.toLowerCase(c2)))
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+
+        }
+        return true;
+    }
+}
